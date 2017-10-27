61a7182a8555942a72dea08cc594fe3af67ddf82
johann <j.k.thairu@gmail.com>
2017-10-27 18:19:26 -0400

    added parsing and gap calculation. Gap calculation is giving weird results- looking into it.

---
+++ generator.py
@@ -0,0 +1,28 @@
+import argparse
+import random
+import time
+import os
+
+
+
+""" calculate gap using the following formula:
+          pkt_size             offered_load
+        --------------    =   ---------------
+        pkt_size + gap           num_node         """
+
+
+def calculate_gap(args):
+    return ((args.pkt_size * args.num_node) / args.offered_load) - args.pkt_size
+
+
+
+"""handling arguments to the program
+expected use is the following:
+python generator.py num_node pkt_size offered_load num_pkts_per_node'[seed] """
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument("num_node", help="The number of nodes used in simulation.", type=int)
+parser.add_argument("pkt_size", help="Size of packets to be  generated.", type=int)
+parser.add_argument("offered_load", help=" measure of the traffic compared to the channel capacity.", type=int)
+parser.add_argument("num_pkts_per_node", help=" Number of packets sent per node in traffic file generated.", type=int)
+parser.add_argument('seed', nargs='?', default=time.time(), type=int)
+args = parser.parse_args()

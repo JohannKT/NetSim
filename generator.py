adbcb85ff593413c7d53ae50e4ddd25fdd45bb91
johann <j.k.thairu@gmail.com>
2017-10-31 12:28:37 -0400

    Made it so that it generates based off of specific node and not a general time stored for the gap. Looks closer to the output he provided.

--- generator.py
+++ generator.py
@@ -11,34 +11,56 @@ import os
         pkt_size + gap           num_node
 """
 
+def getKey(item):
+    return item[0]
+
 def calculate_gap(args):
-    return ((args.pkt_size * args.num_node) / args.offered_load) - args.pkt_size
+    return (((args.pkt_size * args.num_node) / args.offered_load) - args.pkt_size)
 
 def choose_node(args):
-    return random.randint(1,args.num_node)
+    return random.randint(0,args.num_node-1)
 
 def get_send_node(args, cur_node):
-    send = random.randint(1,args.num_node)
+    send = random.randint(0,args.num_node-1)
     while send == cur_node:
-        send = random.randint(1,args.num_node)
+        send = random.randint(0,args.num_node-1)
     return send
+
+def get_packet_id(args, next_packet, node):
+    max_id = args.num_pkts_per_node * args.num_node
+    count = 0
+    temp = random.randint(0,args.num_node-1 - count)
+    while True:
+        if(next_packet[node] + args.num_node > max_id):
+            return 0
+        else:
+            next_packet[node] = next_packet[node] + args.num_node
+            return 1
 
 def generate_file(args, gap):
     # create dict for keep track of packets sent by each node
     packets_sent = {}
+    next_packet = {}
+    node_time = {}
     packets_left = args.num_pkts_per_node * args.num_node
     current_time = 0
-    pkt_id= 1
+    output_array= []
+
+    counter = []
+
     outfile = open("traffic.txt", "w")
     outfile.write(str(packets_left) + "\n")
     #initialize to amount they will each send
     for x in range(0, args.num_node):
-        packets_sent[x+1] = 100
+        packets_sent[x] = args.num_pkts_per_node
+        next_packet[x] = x
+        node_time[x] = 0
 
     #keep going until all values in dict are 0 meaning all have sent their packets
     while  not all(value == 0 for value in packets_sent.values()):
         rand_gap = random.randint(1,2*gap)
         cur_node = 0
+
         while True:
             cur_node = choose_node(args)
             if packets_sent[cur_node] == 0:
@@ -48,16 +70,31 @@ def generate_file(args, gap):
                     cur_node = choose_node(args)
             else:
                 break
+
         send_node = get_send_node(args, cur_node)
-        current_time = current_time + rand_gap
+        pkt_id = next_packet[cur_node]
+        valid = get_packet_id(args, next_packet, cur_node)
+        #if not valid:
+            #continue
+        #update time:
+        node_time[cur_node] = node_time[cur_node] + rand_gap
+        current_time = node_time[cur_node]
+        counter.append(pkt_id)
+        temp.remove(pkt_id)
+        if not valid:
+            if len(output_array) == (args.num_pkts_per_node * args.num_node):
+                break
+
+
+
         packets_sent[cur_node] = packets_sent[cur_node] - 1
-        outfile.write(str(pkt_id) + " " + str(cur_node) + " " + str(send_node) + " "+ str(args.pkt_size) + " "+ str(current_time) + "\n" )
 
-        pkt_id = pkt_id + 1
+        output_array.append(  (current_time, str(pkt_id) + " " + str(cur_node) + " " + str(send_node) + " "+ str(args.pkt_size) + " "+ str(current_time) +"\n") )
 
+    output_array = sorted(output_array, key= getKey)
 
-    print packets_sent
-
+    for x in output_array:
+        outfile.write(x[1])
 
 
 """handling arguments to the program
@@ -72,8 +109,10 @@ parser.add_argument("num_pkts_per_node", help=" Number of packets sent per node 
 parser.add_argument('seed', nargs='?', default=time.time(), type=int)
 args = parser.parse_args()
 
-print "node", choose_node(args)
 
+temp = []
+for x in range(0, args.num_pkts_per_node * args.num_node):
+    temp.append(x)
 
 gap = calculate_gap(args)
 generate_file(args, gap)

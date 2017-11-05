365effac2fa03ae65c5f8c14b76fa5761c4eccab
johann <j.k.thairu@gmail.com>
2017-11-05 14:59:05 -0500

    Simplified packet IDs and casted gap to an int to avoid possible complications

--- generator.py
+++ generator.py
@@ -27,31 +27,18 @@ def get_send_node(num_node, cur_node):
         send = random.randint(0,num_node-1)
     return send
 
-def get_packet_id(num_pkts_per_node, num_node, next_packet, node):
-    '''
-      Packet IDs are determined by using the next_packet dictionary. This dictionary original containes the node number + 1.
-      Each node determines it's next packet id by adding the number of nodes to it's current packet ID.
-      Ex. Node 1's first packet ID will be 2. It's next ID will be 2+num_nodes and it's next ID will be 2+2*num_nodes and so on.
-    '''
-    max_id = num_pkts_per_node * num_node
-    if(next_packet[node] + num_node > max_id):
-        return 0 #failed
-    else:
-        next_packet[node] = next_packet[node] + num_node
-        return 1 #success
-    
+
 def generate_file(num_node, pkt_size, offered_load, num_pkts_per_node, seed, gen_file = True):
     # create dict for keeping track of packets sent by each node
     # Each dict's key is the node number
-    gap = calculate_gap(pkt_size, num_node, offered_load)
-    packets_sent = {} 
-    next_packet = {} 
+    gap = int(calculate_gap(pkt_size, num_node, offered_load))
+    packets_sent = {}
     node_time = {}
     packets_left = num_pkts_per_node * num_node #total number of packets
     current_time = 0
     output_array= []
+    count = 0 #used for creating unique packet IDs
 
-    counter = []
 
     if gen_file:
         outfile = open("traffic.txt", "w")
@@ -59,7 +46,6 @@ def generate_file(num_node, pkt_size, offered_load, num_pkts_per_node, seed, gen
     #initialize to amount they will each send
     for x in range(0, num_node):
         packets_sent[x] = num_pkts_per_node
-        next_packet[x] = x
         node_time[x] = 0
 
     #keep going until all values in dict are 0 meaning all have sent their packets
@@ -78,22 +64,15 @@ def generate_file(num_node, pkt_size, offered_load, num_pkts_per_node, seed, gen
                 break
 
         send_node = get_send_node(num_node, cur_node)
-        pkt_id = next_packet[cur_node]
-        valid = get_packet_id(num_pkts_per_node, num_node, next_packet, cur_node)
-        #if not valid:
-            #continue
-        #update time:
+        pkt_id = count
         node_time[cur_node] = node_time[cur_node] + rand_gap
         current_time = node_time[cur_node]
-        counter.append(pkt_id)
-        if not valid:
-            if len(output_array) == (num_pkts_per_node * num_node):
-                break
+
 
         packets_sent[cur_node] = packets_sent[cur_node] - 1
-
         output_array.append(  (current_time, str(pkt_id) + " " + str(cur_node) + " " + str(send_node) + " "+ str(pkt_size) + " "+ str(current_time) +"\n") )
-
+        count = count + 1
+    #sort by time
     output_array = sorted(output_array, key= getKey)
     if gen_file:
         for x in output_array:
